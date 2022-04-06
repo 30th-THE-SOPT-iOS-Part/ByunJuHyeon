7d209daeb4ad040081c8950e3251e304256ed3e8
juhyeon <wngus4296@swu.ac.kr>
2022-04-07 04:01:21 +0900

    [FEAT] 회원가입 VC 연결

---
+++ Instagram/Instagram/Signup1ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Signup1ViewController.swift
+//  Instagram
+//
+//  Created by 변주현 on 2022/04/07.
+//
+
+import UIKit
+
+class Signup1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ Instagram/Instagram/Signup2ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Signup2ViewController.swift
+//  Instagram
+//
+//  Created by 변주현 on 2022/04/07.
+//
+
+import UIKit
+
+class Signup2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
