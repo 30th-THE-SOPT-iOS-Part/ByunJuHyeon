d7a8397e600b6f01796a37b55adc6abc48b304b3
juhyeon <wngus4296@swu.ac.kr>
2022-04-07 04:08:32 +0900

    [FEAT] 완료 화면 UI 구현 및 VC 연결

---
+++ Instagram/Instagram/SuccessViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SuccessViewController.swift
+//  Instagram
+//
+//  Created by 변주현 on 2022/04/07.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
