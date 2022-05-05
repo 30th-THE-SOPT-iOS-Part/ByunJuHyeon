2e8297ea0815be7cded95ab4ac6e1dbc0b6333dd
juhyeon <wngus4296@swu.ac.kr>
2022-05-05 16:56:09 +0900

    [FEAT] 테이블 뷰 헤더에 컬렉션 뷰 삽입 및 좋아요 버튼 누르면 알럿 뜨게 구현

--- Instagram/Instagram/Controllers/TabBar/FeedTabViewController.swift
+++ Instagram/Instagram/Controllers/TabBar/FeedTabViewController.swift
@@ -9,25 +9,64 @@ import UIKit
 
 class FeedTabViewController: UIViewController {
     
-    // MARK: - Properties
-    @IBOutlet weak var storyCollectionView: UICollectionView!
+    // MARK: - IBOutlet
+    @IBOutlet weak var postTableView: UITableView!
+    
+    // MARK: - Views
+    lazy var storyCollectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        let collectionViewNib = UINib(nibName: StoryCollectionViewCell.identifier, bundle: nil)
+        
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 4
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(collectionViewNib, forCellWithReuseIdentifier: StoryCollectionViewCell.identifier)
+        collectionView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 8 + 72 + 8)
+        collectionView.showsHorizontalScrollIndicator = false
+        
+        return collectionView
+    }()
     
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setStoryCollectionView()
+        setPostTableView()
     }
 }
 
 // MARK: - Custom Methods
 extension FeedTabViewController {
-    private func setStoryCollectionView() {
-        let nib = UINib(nibName: StoryCollectionViewCell.identifier, bundle: nil)
-        storyCollectionView.register(nib, forCellWithReuseIdentifier: StoryCollectionViewCell.identifier)
+    private func setPostTableView() {
+        let nib = UINib(nibName: PostTableViewCell.identifier, bundle: nil)
+        postTableView.register(nib, forCellReuseIdentifier: PostTableViewCell.identifier)
         
-        storyCollectionView.delegate = self
-        storyCollectionView.dataSource = self
+        postTableView.delegate = self
+        postTableView.dataSource = self
+        
+        // 스토리 CollectionView 아래 border
+        let borderBottom = UIView(frame: CGRect(x:0, y:storyCollectionView.bounds.height, width: storyCollectionView.bounds.size.width, height: 1.0))
+        borderBottom.backgroundColor = UIColor.self.init(red: 0.925, green: 0.925, blue: 0.925, alpha: 1.0)
+        postTableView.addSubview(borderBottom)
+        
+        postTableView.tableHeaderView = storyCollectionView
+    }
+}
+
+// MARK: - SendUpdateStatusDelegate
+extension FeedTabViewController: SendUpdateStatusDelegate {
+    func alertLikeStatus(status: Bool) {
+        let message = status ? "like button is selected" : "like button is canceled"
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(confirm)
+        present(alert, animated: true, completion: nil)
     }
 }
 
@@ -49,8 +88,6 @@ extension FeedTabViewController: UICollectionViewDataSource {
         cell.setData(storyData: FeedStoryDataModel.sampleData[indexPath.row])
         return cell
     }
-    
-    
 }
 
 // MARK: - UICollectionViewDelegateFlowLayout
@@ -67,9 +104,32 @@ extension FeedTabViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 4
     }
 }
 
+// MARK: - UITableViewDelegate
+extension FeedTabViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension FeedTabViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return FeedPostDataModel.sampleData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.identifier, for: indexPath) as? PostTableViewCell else { return UITableViewCell() }
+        
+        cell.setData(postData: FeedPostDataModel.sampleData[indexPath.row])
+        
+        cell.clickLikeDelegate = self
+        
+        return cell
+    }
+}
