a656fe219a3d8bc7e341f0718db2c004c98099ec
juhyeon <wngus4296@swu.ac.kr>
2022-05-10 19:13:39 +0900

    [ADD] SigninDataModel 생성

---
+++ Instagram/Instagram/Network/APIModels/Models/Sign/SigninDataModel.swift
@@ -0,0 +1,20 @@
+//
+//  SigninModel.swift
+//  Instagram
+//
+//  Created by 변주현 on 2022/05/10.
+//
+
+import Foundation
+
+// MARK: - DataClass
+struct SigninDataModel: Codable {
+    let name: String
+    let email: String
+
+    // 커스텀 키 매핑
+    // JSON에서 Key는 항상 String이기 때문에 Raw Value타입으로 String 넣어줌
+    enum CodingKeys: String, CodingKey {
+        case name, email
+    }
+}
